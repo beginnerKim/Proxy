944c2fa54c35eb3dec6be3f1c961f950e558c20e
kimbeginner <kim.beginner@gmail.com>
2022-04-29 17:13:39 +0900

    (*) 일부 evn 수정 및 파일 권한 변경

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM nginx:latest
 LABEL maintainer="YTS NGINX Docker Maintainers <kimbeginner@ytstar.co.kr>"
 
 ENV USER_CONFIG_DIR /etc/nginx/extra.config
-
+ENV DEFAULT_DIR /etc/nginx
 
 COPY entrypoint.sh /
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,21 +2,19 @@
 # vim:sw=4:ts=4:et
 set -e
 
-
-NGINX_DIR=/etc/nginx
 if [ -d ${USER_CONFIG_DIR} ]; then
 
     for f in ${USER_CONFIG_DIR}/*.conf
     do
         config=$(basename $f)
         echo "check ${config} file..."
-        if [ -f ${NGINX_DIR}/${config} ]; then
+        if [ -f ${DEFAULT_DIR}/${config} ]; then
             echo "    >>>> remove default ${config} config file"
-            rm ${NGINX_DIR}/${config}
+            rm ${DEFAULT_DIR}/${config}
         fi
 
-        echo "    >>>> copy ${config} config file to ${NGINX_DIR}"
-        install -g root -o root ${USER_CONFIG_DIR}/${config} ${NGINX_DIR}
+        echo "    >>>> copy ${config} config file to ${DEFAULT_DIR}"
+        install -g root -o root ${USER_CONFIG_DIR}/${config} ${DEFAULT_DIR}
     done
 fi
 
